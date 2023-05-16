dab59a36478ff6b122929448bb807e65087dcc38
Eugen <vasilenkoevgen88@gmail.com>
2023-05-16 11:57:27 +0300

    Fix css import for eject

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
-import './Blog.css';
-import post from '../../components/Post/Post';
+import classes from './Blog.css';
+// import post from '../../components/Post/Post';
 
 class Blog extends Component {
     state = {
@@ -45,7 +45,7 @@ class Blog extends Component {
 
         return (
             <div>
-                <section className="Posts">
+                <section className={classes.Posts}>
                     {posts}
                 </section>
                 <section>
