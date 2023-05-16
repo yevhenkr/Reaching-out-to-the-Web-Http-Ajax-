ecfb1b678e9349a857df108834c33fae1174b86b
Eugen <vasilenkoevgen88@gmail.com>
2023-05-16 11:56:41 +0300

    Add eject

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 
-import './FullPost.css';
+import classes from './FullPost.css';
 
 class FullPost extends Component {
     render() {
         let post = <p style={{ textAlign: 'center' }}>Please select a Post!</p>;
         if (this.props.id) {
             post = (
-                <div className="FullPost">
+                <div className={classes.FullPost}>
                     <h1>Title</h1>
                     <p>Content</p>
-                    <div className="Edit">
-                        <button className="Delete">Delete</button>
+                    <div className={classes.Edit}>
+                        <button className={classes.Delete}>Delete</button>
                     </div>
                 </div>
 
