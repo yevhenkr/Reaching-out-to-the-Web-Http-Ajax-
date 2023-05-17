97303d48d24b274b6e51540dff143b45d2b3dc15
Eugen <vasilenkoevgen88@gmail.com>
2023-05-17 14:51:56 +0300

    fix

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { withRouter } from 'react-router-dom';
 
 import classes from './Post.css';
 
-const post = (props) => {
-    console.log(props);
-    return (
-        <article className={classes.Post}
-            onClick={props.clicked}>
-            <h1>{props.title}</h1>
-            <div className="Info">
-                <div className="Author">{props.author}</div>
-            </div>
-        </article>
-    );
-};
+const post = (props) => (
+    <article className={classes.Post}
+        onClick={props.clicked}>
+        <h1>{props.title}</h1>
+        <div className="Info">
+            <div className="Author">{props.author}</div>
+        </div>
+    </article>
+);
 
-export default withRouter(post);
+export default post;
