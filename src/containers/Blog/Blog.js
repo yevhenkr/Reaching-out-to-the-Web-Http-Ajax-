492b51256b52ce3180d1e07a91e77cbf6319c83d
Eugen <vasilenkoevgen88@gmail.com>
2023-05-17 12:31:26 +0300

    Folder restract

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,57 +1,11 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import axios from '../../axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import classes from './Blog.css';
-// import post from '../../components/Post/Post';
+import Posts from './Posts/Posts'
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false
-    }
-
-
-    componentDidMount() {
-        axios.get('/posts')
-            .then(response => {
-                const posts = response.data.slice(0, 4);
-                const updatePosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: 'Max'
-                    }
-                });
-                this.setState({ posts: updatePosts });
-                // console.log(response);
-            })
-            .catch(error => {
-                this.setState({ error: true });
-            });
-    }
-
-    postSelectHandler = (id) => {
-        this.setState({ selectedPostId: id });
-    }
-
     render() {
-        let posts = <p style={{ textAlign: 'center' }}>Somthing went wrong</p>;
-        if (!this.state.error) {
-
-            posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    clicked={() => this.postSelectHandler(post.id)}
-                />
-            });
-        }
-
         return (
             <div className={classes.Blog}>
                 <header>
@@ -62,9 +16,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className={classes.Posts}>
-                    {posts}
-                </section>
+                <Posts />
             </div>
         );
     }
