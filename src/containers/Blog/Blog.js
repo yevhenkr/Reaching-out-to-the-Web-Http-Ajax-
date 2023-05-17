62bfa6125895780527068aa674f652846189ba52
Eugen <vasilenkoevgen88@gmail.com>
2023-05-17 11:05:32 +0300

    191 Added two links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -53,7 +53,15 @@ class Blog extends Component {
         }
 
         return (
-            <div>
+            <div className={classes.Blog}>
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/new-post'>New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className={classes.Posts}>
                     {posts}
                 </section>
