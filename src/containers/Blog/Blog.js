90543f91e8228cdc73726ab2b217635e58cb3d95
Eugen <vasilenkoevgen88@gmail.com>
2023-05-17 16:14:56 +0300

    202 Passing Route Parameters

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import { NavLink, Route } from 'react-router-dom';
 import classes from './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from '../Blog/NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
     render() {
@@ -31,8 +32,9 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                    <Route path="/" component={Posts} />
+                    <Route path="/" exact component={Posts} />
                     <Route path="/new-post" component={NewPost} />
+                    <Route path="/id" exact component={FullPost} />
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from '../../../axios';
+import { Link } from 'react-router-dom';
+
+
 import Post from '../../../components/Post/Post';
 import './Posts.css';
 
@@ -35,12 +38,13 @@ class Posts extends Component {
         if (!this.state.error) {
 
             posts = this.state.posts.map(post => {
-                return <Post
-                    key={post.id}
+                return (
+                <Link  to={'/' + post.id} key={post.id}>
+                <Post
                     title={post.title}
                     author={post.author}
                     clicked={() => this.postSelectHandler(post.id)}
-                />
+                /></Link>);
             });
         }
 
