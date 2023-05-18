95720257a54a4084fc70361db334ada920ec9845
Eugen <vasilenkoevgen88@gmail.com>
2023-05-18 10:47:13 +0300

    212 Working with Guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,6 +8,9 @@ import NewPost from './NewPost/NewPost';
 import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
+    state={
+        auth: false
+    }
     render() {
         return (
             <div className="Blog">
@@ -33,7 +36,7 @@ class Blog extends Component {
                 {/* <Route path="/" exact render={() => <h1>Home</h1>} />
                 <Route path="/" render={() => <h1>Home 2</h1>} /> */}
                 <Switch>
-                    <Route path="/new-post" component={NewPost} />
+                    {this.state.auth ? <Route path="/new-post" component={NewPost} /> : null }
                     <Route path="/posts" component={Posts} />
                     <Redirect from="/" to="/posts"/>
                     {/* <Route path="/" component={Posts} /> */}
